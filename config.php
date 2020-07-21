1649218f5c508fd458b3459823f559d9a4987b12
DD <68047131+xF346x@users.noreply.github.com>
2020-07-21 17:53:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5";
-$phpsesid = "s90rg0mek6rlq21is1ph349782";
-$cfduid = "d7b084d25289b6ee2f9304cbe48bf51bd1595053617";
+$useragent = "____"; // ISI USER AGENT
+$phpsesid = "_____"; // ISI PHPSESID
+$cfduid = "_______"; // ISI CFDUID
